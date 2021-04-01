fa0a2c94748aedef389e816a55a63d41a27e1fb2
AsyncXeNo <xeno25502@gmail.com>
2021-04-01 14:39:42 +0530

    added css

--- src/index.js
+++ src/index.js
@@ -1,36 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// CSS
+
+import './index.css';
 
 // stateless functional component
 // always returns JSX
 
 function BookList () {
   return (
-    <section>
+    <section className='booklist'>
       <Book />
       <Book />
     </section>
-  )
-}
+  );
+};
 
 // components
 
 function Book () {
-  return <article>
-    <Image />
-    <Title />
-    <Author />
-  </article>
-}
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
 function Image () {
-  return <img src='https://images-na.ssl-images-amazon.com/images/I/41mgb2RWkRL._SX346_BO1,204,203,200_.jpg' alt=''></img>
-}
+  return <img src='https://images-na.ssl-images-amazon.com/images/I/41mgb2RWkRL._SX346_BO1,204,203,200_.jpg' alt=''></img>;
+};
 
-function Title () { return <h1>Dvorestsky's Endgame Manual</h1> }
+function Title () { return <h1>Dvorestsky's Endgame Manual</h1>; }
 
-function Author () { return <h4>Dvorestsky</h4> }
+function Author () { return <h4>Dvorestsky</h4>; }
 
 // adding the BookList component in the root div
 
-ReactDOM.render(<BookList />, document.getElementById('root'))
+ReactDOM.render(<BookList />, document.getElementById('root'));
