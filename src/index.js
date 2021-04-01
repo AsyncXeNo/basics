9aa609a3cc3932232fadc454304ea4b6f74b0777
AsyncXeNo <xeno25502@gmail.com>
2021-04-01 14:46:09 +0530

    wtf

--- src/index.js
+++ src/index.js
@@ -39,32 +39,4 @@ function Author () { return <h4>Dvorestsky</h4>; }
 
 // adding the BookList component in the root div
 
-ReactDOM.render(<BookList />, document.getElementById('root'));
-  return (
-    <section>
-      <Book />
-    </section>
-  )
-}
-
-// components
-
-function Book () {
-  return <article>
-    <Image />
-    <Title />
-    <Author />
-  </article>
-}
-
-function Image () {
-  return <img src='https://images-na.ssl-images-amazon.com/images/I/41mgb2RWkRL._SX346_BO1,204,203,200_.jpg' alt=''></img>
-}
-
-function Title () { return <h1>Dvorestsky's Endgame Manual</h1> }
-
-function Author () { return <h4>Dvorestsky</h4> }
-
-// adding the BookList component in the root div
-
 ReactDOM.render(<BookList />, document.getElementById('root'))
